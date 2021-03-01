54d135d4fe9a7b1ddb08f2bd5f1e87d56220df3f
klaoun <36087587+klaoun@users.noreply.github.com>
2021-03-01 20:16:10 +0100

    Update essai_structure.sql

--- install/essai_structure.sql
+++ install/essai_structure.sql
@@ -1,1 +1,9 @@
-
+-- Dump sql -- 
+-- Table structure pour la table 'varbouton'
+DROP TABLE IF EXISTS `varbouton`;
+CREATE TABLE `varbouton` (
+  `id` tinyint(1) NOT NULL auto_increment,
+  `bouton` varchar(17) collate latin1_general_ci NOT NULL,
+  `urlbouton` varchar(102) collate latin1_general_ci NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM
